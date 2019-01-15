580db278d0c04a8a23e63e8ea39dc4bc70a31c49
tshadwel <tshadwel@amazon.com>
2019-01-15 01:14:27 +0000

    good attempt with d3 arcs

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-d3-sun-indicator'
+import SunIndicator from 'react-d3-sun-indicator'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {time: new Date()}
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => this.setState({
+      time: new Date(+this.state.time + 60 * 60 * 60 * .5)
+    }), 10)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <SunIndicator
+          time={this.state.time}
+          style={{width: "50vw", height: "50vh"}} />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,93 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import D3 from 'reactive-d3';
+import * as d3 from 'd3';
 
-import styles from './styles.css'
+import styles from './styles.css';
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+const secondsInADay = 24 * 60 * 60;
+export default class SunIndicator extends React.Component {
+  join({main, width, height}) {
+    let {time = new Date(), bgc = "white", fgc = "black"} = this.props;
+    let svg = d3.select(main);
+    svg.attr("viewBox", `0 0 ${width} ${height}`);
+
+    let seconds = time.getHours()*60*60 + time.getMinutes() * 60 + time.getSeconds();
+
+    let ratio = seconds / secondsInADay;
+
+    let phases = [
+      {name: "before", value: 100 * ratio, class: styles.travelled},
+      {name: "after", value: 100 * (1 - ratio), class: styles.remaining}
+    ];
+
+    const arcs = d3.pie().sort(null).value(d => d.value)
+      .startAngle(Math.PI* (-1/2) ).endAngle(Math.PI / 2)(phases);
+
+    const r = Math.min(width, height)/2-1;
+    const arc = d3.arc().innerRadius(0).outerRadius(r);
+
+    const g = d3.select("."+styles.centre);
+
+    g.attr("transform", `translate(${width /2},${height/2})`)
+
+    // take *just* the arc, rather than the segment ... just delete everything
+    // past L
+    const justArc = (...etc) => arc(...etc).replace(/L.*$/g, "");
+
+    let segments = g.selectAll("path").data(arcs);
+
+    segments.exit().remove();
+
+    segments = segments.enter().append("path")
+        .merge(segments)
+        .attr("fill", d => "none")
+        .attr("stroke", d => fgc)
+        .attr("d", justArc)
+        .attr("class", d => d.data.class)
+        .each((d, i, n) => {
+          let titles = d3.select(n[i]).selectAll("title");
+
+          titles.exit().remove();
+
+          titles.enter()
+            .append("title")
+            .merge(titles)
+            .text(d => d.data.name);
+        });
+
+
+
+    // draw a sunny circle
+    const passedPath = segments.filter("." + styles.travelled).attr("d");
+    const [finalX, finalY] = /[\d-.e]+,[\d-.e]+$/.exec(passedPath)[0].split(",");
+
+    let circles = g.selectAll("circle").data([[finalX, finalY]]);
+
+    circles.exit().remove();
+
+    circles = circles.enter()
+      .append("circle")
+      .merge(circles)
+      .attr("fill", "white")
+      .attr("stroke", "black")
+      .attr("class", styles.sun)
+      .attr("cx", ([x, y]) => x)
+      .attr("cy", ([x, y]) => y)
+      .attr("r", r / 20);
+
+
   }
 
   render() {
-    const {
-      text
-    } = this.props
+    let {className, time, ...etc} = this.props;
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+    return <D3 className={styles.indicator + (className || "")}
+      {...etc} join={({...etc}) => this.join(etc)}>
+
+      <svg>
+        <g className={styles.centre} />
+      </svg>
+    </D3>
   }
 }
+
